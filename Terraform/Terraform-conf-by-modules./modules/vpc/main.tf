57cd9370d37ed5de930751fb0c3d4d844dfc5160
Mostafayouni <105316729+Mostafayouni@users.noreply.github.com>
2024-05-29 17:11:26 +0300

    Create main.tf

---
+++ Terraform/Terraform-conf-by-modules./modules/vpc/main.tf
@@ -0,0 +1,10 @@
+# modules/vpc/main.tf
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.public_subnet_cidr_block
+  availability_zone = var.availability_zone
+}
